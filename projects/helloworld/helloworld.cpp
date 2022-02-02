d05b1acb6a8967e25d1ce6d0d35671b1ac2149c6
jbarret <jbarret@nanoxplore.com>
2022-02-02 17:03:25 +0100

    classe minimum vertex + main enum

--- projects/helloworld/helloworld.cpp
+++ projects/helloworld/helloworld.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include "vertex.h"
+#include "assert.h"
 
 using namespace std;
 
@@ -24,6 +25,35 @@ int main()
         cout << s << " ";
     }    
     cout << endl;
+
+
+    
+    enum type {_1d = 1, _2d = 2, _3d = 3};
+    type dim = _2d;
+    const string pretexte = "\ttraitement";
+    switch(dim)
+    {
+        
+        case _1d:
+            std::cout << pretexte + "\t1d" << std::endl;
+            break;
+        case _2d:
+            std::cout << pretexte + "\t2d" << std::endl;
+            break;
+        case _3d:
+            std::cout << pretexte + "\t3d" << std::endl;
+            break;
+        default:
+            break;
+
+    }
+    
+    
+    typedef Vertex<double> tvd;
+    //tvd *ptvd = new tvd[10];
+    //ptvd[0].name = "O",ptvd[0].x = 0.0; ptvd[0].y = 0.0;
+    //vector<tvd> p;
+    //p.push_back(ptvd[0]);
 
 
 
--- projects/helloworld/vertex.h
+++ projects/helloworld/vertex.h
@@ -2,11 +2,31 @@
 #include <cassert>
 #include <string>
 
+
+
+template<class T>
+T MAX(T& a, T& b)
+{
+    return a > b ? a : b;
+}
+
 template<class T>
 class Vertex
 {
     public:
     T x,y;
+    std::string name;
+
+    Vertex(/**/); // default constructor
+    Vertex(std::string& nname, T& a, T&b):name(nname),x(a),y(b){} //constructor
+    Vertex(const Vertex<T>& v) = default; //copy constructor
+    Vertex<T>& operator= (const Vertex<T>& v) = default;//copy assignment
+    Vertex<T>  operator+ (const Vertex<T>& v);
+    Vertex<T>  operator- (const Vertex<T>& v);
+    Vertex<T>& operator+=(const Vertex<T>& v);
+    Vertex<T>& operator-=(const Vertex<T>& v);
+
+
     
 
 
